0cba98847de53138ff23ba499567cf511f24e34e
timpwbaker <timothy_baker@hotmail.co.uk>
2016-07-19 21:37:37 +0100

    merge rubocop with master

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -3,4 +3,9 @@ FactoryGirl.define do
   	association :user
     message_body "A sample message for testing the submission of a new message"
   end
+
+  factory :long_message do
+  	association :user
+  	message_body "A message that is a lot more than 150 characters, maybe the user isn't being consice enough. Maybe they've got a lot to say. Maybe the UX isn't clear enough. Who knows? It could be anything."
+  end
 end
--- spec/features/messages/post_message_spec.rb
+++ spec/features/messages/post_message_spec.rb
@@ -3,15 +3,24 @@ require 'rails_helper'
 #   As a visitor
 #   I want to visit a home page
 #   So I can learn more about the website
-feature 'Home page', :js => true do
+feature 'Post Messages', :js => true do
 
   # Scenario: Visit the home page
   #   Given I am a visitor
   #   When I visit the home page
   #   Then I see "Welcome"
-  scenario 'visit the home page' do
-  	@user = FactoryGirl.create(:user)
-  	message = FactoryGirl.create(:message, user_id: @user.id)
+  scenario 'post successful message' do
+    @user = FactoryGirl.create(:user)
+    message = FactoryGirl.create(:message, user_id: @user.id)
+    visit root_path
+    fill_in 'message_message_body', :with => 'Sample Message'
+    page.find(".send-message").click
+    expect(page).to have_content("New message:")
+  end
+
+  scenario 'try to create unsuccessful message' do
+    @user = FactoryGirl.create(:user)
+    message = FactoryGirl.create(:long_message, user_id: @user.id)
     visit root_path
     fill_in 'message_message_body', :with => 'Sample Message'
     page.find(".send-message").click
