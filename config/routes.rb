37f2bb5193b63da15f54df35afc09d2a4fb49dfc
timpwbaker <timothy_baker@hotmail.co.uk>
2016-07-21 08:11:42 +0100

    restrict routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :messages
+  resources :messages, only: [:index, :create, :destroy]
   resources :users, only: [:show]
   root to: 'messages#index'
 end
